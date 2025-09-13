3866c473548be29645ca3e699b24b941ff50fa07
zabackary <137591653+zabackary@users.noreply.github.com>
2025-09-13 21:27:31 +0900

    Finish switch.sh

--- switch.sh
+++ switch.sh
@@ -5,10 +5,12 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-if [ -z "$(git status --porcelain)" ]; then 
-  # nixos-rebuild switch --flake .# --use-remote-sudo
-  echo clean
+if [ -z "$(git status --porcelain)" ]; then
+  echo "Switching NixOS configuration..."
+  nixos-rebuild switch --flake .# --use-remote-sudo
+  echo "NixOS configuration switched successfully. Remember to `git push` your changes."
 else 
-  echo Uncommitted changes
+  echo "There are uncommitted changes in the repository. Please commit or stash them before switching configurations."
+  exit 1
 fi
 
