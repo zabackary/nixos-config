3b9059780c6e7cf350de70ddd56dd637d5fca115
zabackary <137591653+zabackary@users.noreply.github.com>
2025-09-21 15:24:39 +0900

    fix: that didnt work

--- hosts/shinjitsu/default.nix
+++ hosts/shinjitsu/default.nix
@@ -126,7 +126,6 @@
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-  inputs.nixpkgs-unstable.config.allowUnfree = true;
 
   # Enable the Flakes feature and the accompanying new nix command-line tool
   nix.settings.experimental-features = [
