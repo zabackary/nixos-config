f0456a3a1806f342c9056401458feb3a0e3757f8
zabackary <137591653+zabackary@users.noreply.github.com>
2025-10-23 22:05:46 +0900

    feat: add v4l2loopback for OBS

--- hosts/shinjitsu/default.nix
+++ hosts/shinjitsu/default.nix
@@ -200,6 +200,15 @@
     binfmt = true;
   };
 
+  # https://nixos.wiki/wiki/OBS_Studio
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    v4l2loopback
+  ];
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+  '';
+  security.polkit.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
