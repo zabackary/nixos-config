b8248265b4fd5e416dab270e7ab570e80075349e
zabackary <137591653+zabackary@users.noreply.github.com>
2025-12-07 20:31:44 +0900

    fix: update nixos-rebuild command to use --sudo instead of --use-remote-sudo

--- switch.sh
+++ switch.sh
@@ -11,14 +11,18 @@ while [[ "$#" -gt 0 ]]; do
 done
 
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
-   exit 1
+  echo "This script must be run as root" 
+  exit 1
 fi
 
 if [ -z "$(git status --porcelain)" ] || [[ $ALLOW_DIRTY -eq 1 ]]; then
   echo "Switching NixOS configuration..."
-  nixos-rebuild switch --flake .# --use-remote-sudo
-  echo "NixOS configuration switched successfully. Remember to \`git push\` your changes."
+  nixos-rebuild switch --flake .# --sudo
+  if [[ $? -eq 0 ]]; then
+   echo "NixOS configuration switched successfully. Remember to \`git push\` your changes."
+  else
+   exit 1
+  fi
 else 
   echo "There are uncommitted changes in the repository. Please commit or stash them before switching configurations."
   exit 1
