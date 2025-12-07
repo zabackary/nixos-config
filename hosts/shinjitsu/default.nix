78e5014256ccfde26ad33baf44773aa9ba959f20
zabackary <137591653+zabackary@users.noreply.github.com>
2025-12-07 20:23:02 +0900

    chore: make OBS camera name more evident

--- hosts/shinjitsu/default.nix
+++ hosts/shinjitsu/default.nix
@@ -214,7 +214,7 @@
     v4l2loopback
   ];
   boot.extraModprobeConfig = ''
-    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+    options v4l2loopback devices=1 video_nr=1 card_label="OBS Virtual Camera" exclusive_caps=1
   '';
   security.polkit.enable = true;
 
