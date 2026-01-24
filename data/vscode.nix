4733f0cb896ef3b5c75f643794310ac9acbc0806
zabackary <137591653+zabackary@users.noreply.github.com>
2026-01-24 20:39:28 +0900

    chore: bump essentials

--- data/vscode.nix
+++ data/vscode.nix
@@ -1,4 +1,4 @@
 {
-  sha256 = "sha256-s4Q2Cqzt1lqUwF9Fgk7dUf4jMhLPHS5DvILAKq3qBlY=";
-  version = "1.108.1";
+  sha256 = "sha256-3C4sYGYEx327Ej1euMKappfeAwdD5Ar7p0i7f/efNWw=";
+  version = "1.108.2";
 }
