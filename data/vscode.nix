ef8a64e1ecf3e88a012cf0a72cd8dc0c0fc29054
zabackary <137591653+zabackary@users.noreply.github.com>
2026-03-08 15:37:22 +0900

    chore: update

--- data/vscode.nix
+++ data/vscode.nix
@@ -1,4 +1,4 @@
 {
-  sha256 = "sha256-ruZOXIZfK9t61GxaoEvruAgGbxCbFTNlo6dXlCd4SmA=";
-  version = "1.110.0";
+  sha256 = "sha256-yK/nsjj6wzW9Oc9rSwD1oX4c8HyI0s8cqb7SWhNw65Y=";
+  version = "1.110.1";
 }
