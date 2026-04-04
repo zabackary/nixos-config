62b1955ee2cfc9d8c969bef738a3e2359801ccaa
zabackary <137591653+zabackary@users.noreply.github.com>
2026-04-04 19:02:03 +0900

    feat: fprintd

--- hosts/shinjitsu/default.nix
+++ hosts/shinjitsu/default.nix
@@ -254,6 +254,9 @@
     '';
   };
 
+  # Fingerprint
+  services.fprintd.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
