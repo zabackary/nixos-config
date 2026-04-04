97cea589e27a38cab4cf86be79c5ec13a67b3535
zabackary <137591653+zabackary@users.noreply.github.com>
2026-04-04 19:02:07 +0900

    chore: clean up

--- hosts/shinjitsu/default.nix
+++ hosts/shinjitsu/default.nix
@@ -259,11 +259,7 @@
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.mtr.enable = true;
 
   # List services that you want to enable:
 
@@ -273,8 +269,6 @@
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
